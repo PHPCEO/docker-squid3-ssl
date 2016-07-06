97c740945ab0710c30fd891e247172188b98b974
genevera <jltgnyc+github@gmail.com>
2016-07-06 02:10:37 -0400

    fix hostname

--- get_install_keys.sh
+++ get_install_keys.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sleep 2;
-docker cp squid_ssl_proxy:/etc/squid/certs/squid-ssl.docker.crt $(pwd)/squid-ssl.docker.crt;
-docker cp squid_ssl_proxy:/etc/squid/certs/private.pem $(pwd)/private.pem;
+docker cp squid-ssl-proxy:/etc/squid/certs/squid-ssl.docker.crt $(pwd)/squid-ssl.docker.crt;
+docker cp squid-ssl-proxy:/etc/squid/certs/private.pem $(pwd)/private.pem;
